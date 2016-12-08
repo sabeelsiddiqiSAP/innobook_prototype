0fcaed2d617a9c647a10d71174ccd3306a1eeb97
sabeelsiddiqiSAP <sabeelsiddiqi@sabeelsiddiqi.local>
2016-12-08 02:14:47 -0500

    s3 paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,11 +82,12 @@ Rails.application.configure do
 
   # For Image Hosting on Amazon S3
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 end
